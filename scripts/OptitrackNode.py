eaeb6c9a081d450dd377cdf2920a994a12534e38
Marko K <marko.krizmancic@gmail.com>
2016-11-04 19:03:15 +0100

    Optitrack node: corrected rotation axis calculation and added docstrings Other: created launch file

--- scripts/OptitrackNode.py
+++ scripts/OptitrackNode.py
@@ -17,57 +17,66 @@ from math import degrees
 class OptitrackNode():
 
     def quat_to_eul (self, data):
-        # Funcion that transforms quaternions to euler
+        """ 
+        This function takes data in quaternion form, exchanges y and z axis and 
+        transforms the data to euler form
+        """
         quaternion = (
             data.pose.orientation.x,
-            data.pose.orientation.y,
             data.pose.orientation.z,
+            data.pose.orientation.y,
             data.pose.orientation.w)
 
         return euler_from_quaternion(quaternion)
 
     def adjust_axis_1 (self,data):
-        # Function that adjusts position info for our UAV
-        self.uav1_pos.x = data.pose.position.x
-        self.uav1_pos.y = data.pose.position.z
-        self.uav1_pos.z = data.pose.position.y
+        """ 
+        Callback function that adjust axis data for our UAV.
+        Data is stored in class variables.
+        """
+        self.uav1.pos.x = data.pose.position.x
+        self.uav1.pos.y = data.pose.position.z
+        self.uav1.pos.z = data.pose.position.y
 
         euler = self.quat_to_eul(data)
-        self.uav1_rot.roll = degrees(euler)[0]
-        self.uav1_rot.pitch = degrees(euler)[2]
-        self.uav1_rot.yaw = degrees(euler)[1]
+        self.uav1.rot.roll = degrees(euler[0])
+        self.uav1.rot.pitch = degrees(euler[1])
+        self.uav1.rot.yaw = degrees(euler[2])
 
     def adjust_axis_2 (self,data):
-        # Function that adjusts position info for opponent UAV
-        self.uav2_pos.x = data.pose.position.x
-        self.uav2_pos.y = data.pose.position.z
-        self.uav2_pos.z = data.pose.position.y
+        """ 
+        Callback function that adjust axis data for opponent UAV.
+        Data is stored in class variables.
+        """
+        self.uav2.pos.x = data.pose.position.x
+        self.uav2.pos.y = data.pose.position.z
+        self.uav2.pos.z = data.pose.position.y
 
         euler = self.quat_to_eul(data)
-        self.uav2_rot.roll = degrees(euler[0])
-        self.uav2_rot.pitch = degrees(euler[2])
-        self.uav2_rot.yaw = degrees(euler[1])
+        self.uav2.rot.roll = degrees(euler[0])
+        self.uav2.rot.pitch = degrees(euler[1])
+        self.uav2.rot.yaw = degrees(euler[2])
 
     # Must have __init__(self) function for a class
     def __init__(self):
         # Create a publisher
-        pub_p1 = rospy.Publisher('MyUAV/pose', CustomPose, queue_size=1)
-        pub_p2 = rospy.Publisher('OpUAV/pose', CustomPose, queue_size=1)
+        pub_p1 = rospy.Publisher('MyUAV/cpose', CustomPose, queue_size=1)
+        pub_p2 = rospy.Publisher('OpUAV/cpose', CustomPose, queue_size=1)
  
         # Set the message to publish as command.
-        self.uav1_pos = CustomPose()    # Position of our UAV
-        self.uav2_pos = CustomPose()    # Position of opponent UAV
+        self.uav1 = CustomPose()    # Position of our UAV
+        self.uav2 = CustomPose()    # Position of opponent UAV
         
         # Create subscribers
-        rospy.Subscriber("/vrpn_client_node/kruna/pose", PoseStamped, self.adjust_axis_1)
-        rospy.Subscriber("/vrpn_client_node/UAV2/pose", PoseStamped, self.adjust_axis_2)
+        rospy.Subscriber("MyUAV/pose", PoseStamped, self.adjust_axis_1)
+        rospy.Subscriber("OpUAV/pose", PoseStamped, self.adjust_axis_2)
         
         # Main while loop.
         while not rospy.is_shutdown():
             # Publish our command.
-            pub_p1.publish(self.uav1_pos)
-            pub_p2.publish(self.uav2_pos)
-            rospy.sleep(1.0)
+            pub_p1.publish(self.uav1)
+            pub_p2.publish(self.uav2)
+            rospy.sleep(0.1)
 
 if __name__ == '__main__':
     # Initialize the node and name it.
