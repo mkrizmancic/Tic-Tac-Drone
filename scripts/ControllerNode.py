6f40c6d6d32cbc6c13e9af30052dcb047ffb6750
Marko K <marko.krizmancic@gmail.com>
2016-12-07 23:29:47 +0100

    Controller Node: 	added callback for joystick input (still missing values) 	made a general name for publishing message 	added node explanation docstring complete.launch launch file: 	added entry for Controller Node

--- scripts/ControllerNode.py
+++ scripts/ControllerNode.py
@@ -11,39 +11,55 @@ import rospy
 from sensor_msgs.msg import Joy
 
 
-class NodeExample():
+class ControllerNode():
     # Callback for msgs
-    def callback(self,data):
-        
+    def joystick_callback(self,data):
+        if (data.GUMB_ZA_PROMJENU_MODA):
+            self.mode = not self.mode
+
+        self.IME_PORUKE.pitch = data.axes[] / self.max_axis_value * (1-0.6*self.mode)
+        self.IME_PORUKE.roll = data.axes[] / self.max_axis_value * (1-0.6*self.mode)
+        self.IME_PORUKE.throttle = data.axes[] / self.max_axis_value * (1-0.6*self.mode)
+        self.IME_PORUKE.yaw = data.axes[] / self.max_axis_value * (1-0.6*self.mode)
+        #ili
+        self.IME_PORUKE.yaw = data.buttons[]*self.yaw_rate*(-1) + data.buttons[]*self.yaw_rate
+
             
     # Must have __init__(self) function for a class
     def __init__(self):
         # Create a publisher for commands
-        pub = rospy.Publisher('',,queue_size=1)
-        
+        pub = rospy.Publisher('manual_output',TIP_PORUKE, queue_size=1)
+
+        # Local helper variables
+        self.mode = 0 # Modifier for controls sensitivity 0 -> 100%, 1 -> 40%
+        self.max_axis_value =
+        self.yaw_rate =
+
         # Set the message to publish as command.
-        # self.variable means you can access it from class fnc
-        self. = 
+        self.IME_PORUKE = TIP_PORUKE()
         
         # Initialize message variables.
-
+        self.IME_PORUKE.pitch = 0
+        self.IME_PORUKE.roll = 0
+        self.IME_PORUKE.yaw = 0
+        self.IME_PORUKE.throttle = 0
         
         # Create a subscriber for color msg
-        rospy.Subscriber("", Joy, self.callback)
+        rospy.Subscriber("joystick_input", Joy, self.joystick_callback)
         
         # Main while loop.
         while not rospy.is_shutdown():
             # Publish our command.
-            pub.publish(self.)
+            pub.publish(self.IME_PORUKE)
             rospy.sleep(1.0)
 
 if __name__ == '__main__':
     # Initialize the node and name it.
-    rospy.init_node('pyclass')
+    rospy.init_node('Controller')
     
     # Go to class functions that do all the heavy lifting.
     # Do error checking.
     try:
-        ne = NodeExample()
+        co = ControllerNode()
     except rospy.ROSInterruptException:
         pass
